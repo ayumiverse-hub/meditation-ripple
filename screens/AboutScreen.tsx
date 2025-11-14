36c5ad81e6d4ad9e21ffa8ed295593239b337130
ayuko399 <ayumikonishi8970@gmail.com>
2025-11-14 23:00:55 +0000

    fix: fix typo in AboutScreen

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -17,7 +17,7 @@ export default function AboutScreen() {
   );
 }
 
-const styles = StyleSheet.creat({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#e0f2fe",
